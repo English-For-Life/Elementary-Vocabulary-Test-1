7beaf8ab61ad2b7c8d1e552eb1feefe28724bd4e
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-28 10:44:21 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"We pick things up with our ______ .",
+        options:["hands", "arms", "hair", "heads"],
+        answer: 0
+    },
+    {
+        q:"I lick an ice-cream with my ______ .",
+        options:["lips", "tongue", "chest", "knee"],
+        answer: 1
+    },
+    {
+        q:"To eat something I put it in my ______ .",
+        options:["neck", "nose", "mouth", "elbow"],
+        answer: 2
+    },
+    {
+        q:"We comb and brush our ______ .",
+        options:["fingers", "shoulder", "sole", "hair"],
+        answer: 3
+    },
+    {
+        q:"I brush my ______ regularly, especially after eating.",
+        options:["teeth", "thumb", "lips", "waist"],
+        answer: 0
+    },
+    {
+        q:"I sometimes go to school ______ bus.",
+        options:["to", "by", "in", "at"],
+        answer: 1
+    },
+    {
+        q:"She waited ______ the bus at the bus stop.",
+        options:["with", "of", "for", "to"],
+        answer: 2
+    },
+    {
+        q:"She has got a yellow dress ______ .",
+        options:["to", "with", "in", "on"],
+        answer: 3
+    },
+    {
+        q:"Who is the man ______ the red tie?",
+        options:["with", "in", "off", "on"],
+        answer: 0
+    },
+    {
+        q:"I remember her. She was the one ______ green shoes.",
+        options:["by", "with", "on", "at"],
+        answer: 1
+    }
+
+
+
+]
